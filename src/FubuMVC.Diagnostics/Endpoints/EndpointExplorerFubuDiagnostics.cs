da5a9a7fda71c594823e6860a554574bd67d9ed9
jeremydmiller <jeremydmiller@yahoo.com>
2013-12-10 09:53:54 -0600

    using DiagnosticChain for 'is not diagnostic chain' test.  Closes GH-57

--- src/FubuMVC.Diagnostics/Endpoints/EndpointExplorerFubuDiagnostics.cs
+++ src/FubuMVC.Diagnostics/Endpoints/EndpointExplorerFubuDiagnostics.cs
@@ -4,6 +4,7 @@ using FubuMVC.Core.Registration;
 using FubuMVC.Core.Registration.Nodes;
 using FubuMVC.Core.Urls;
 using FubuMVC.Core.View;
+using FubuMVC.Diagnostics.Model;
 using FubuMVC.Diagnostics.Runtime;
 
 namespace FubuMVC.Diagnostics.Endpoints
@@ -36,19 +37,7 @@ namespace FubuMVC.Diagnostics.Endpoints
 
         public static bool IsNotDiagnosticRoute(BehaviorChain chain)
         {
-            if (chain.Route != null)
-            {
-                if (chain.Route.Pattern.Contains("_data/" + typeof(RequestLog).Name)) return false;
-                if (chain.Route.Pattern.Contains("_data/" + typeof(RouteReport).Name)) return false;
-
-                return !chain.Route.Pattern.Contains(DiagnosticsRegistration.DIAGNOSTICS_URL_ROOT);
-            }
-
-            // TODO -- figure out how to do this again
-            //            if (chain.As<ITracedModel>().AllEvents().OfType<ChainImported>().Any(x => x.Source.ProvenanceChain.OfType<ServiceRegistryProvenance>().Any(x => x.))
-            //            {
-            //                return false;
-            //            }
+            if (chain is DiagnosticChain) return false;
 
 
             if (chain.Calls.Any(x => x.HandlerType.Assembly == Assembly.GetExecutingAssembly()))
